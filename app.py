d830d4e8198a791f4f49ca7424521f128b98e7c9
ImranUPM <214764@student.upm.edu.my>
2025-06-26 01:13:29 +0800

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title('Mental Health in Tech Survey Data Visualization')
-st.header('Combined 2014 and 2016 Data')
-df_combined = pd.read_csv('combined_mental_health_data.csv')
-st.dataframe(df_combined)
